0abe24c046beb520fc6eb168c0ea9a32002ee06f
Gennady <genius163sam@gmail.com>
2019-09-07 13:00:52 +0400

    feat(dijkstra): Add some tests for "Dijkstra algorithm"

---
+++ dijkstra-algorythm/src/main/java/com/bobocode/exceptions/IncorrectDistanceException.java
@@ -0,0 +1,7 @@
+package com.bobocode.exceptions;
+
+public class IncorrectDistanceException extends RuntimeException {
+    public IncorrectDistanceException(String message) {
+        super(message);
+    }
+}
